4733995880a06fa53ea5b7880033342c415979d6
sebastian.daloia <sebastiandaloia@gmail.com>
2020-11-14 12:09:45 -0300

    Funcion CheckProp reprogramada

--- PROP/prop.py
+++ PROP/prop.py
@@ -1,91 +1,116 @@
 # -*- coding: utf-8 -*-
-import re
+import re;
+import numpy as  np;
+import pandas as pd;
+from sklearn.metrics import f1_score
 
-binConnectives = '↔|→|∨|∧';
-unConnective   = '¬';
-atom            = 'p\d+';
-falsum          = '⊥';
-
-atomicMatch  = '^%s$'%(atom);
-falsumMatch  = '^%s$'%(falsum);
-binConnMatch = '^\((\(.+?\)|p\d+|⊥)(%s)(\(.+?\)|p\d+|⊥)\)$'%(binConnectives);
-unConnMatch  = '^\((%s)(\(.+?\)|p\d+|⊥)\)$'%(unConnective);
-
-class Tree:
-  def __init__(self, root, left=None, right=None):
-    self.root = root;
-    self.left = left;
-    self.right = right;
-
-  def printTree(self, tab=''):
-    print("%s%s \n"%(tab, self.root));
-    if self.left:
-      self.left.printTree(tab = tab + '   ');
-    if self.right:
-      self.right.printTree(tab = tab + '   ');
-
-def parseTree(phi):
-  if re.match(atomicMatch, phi) or re.match(falsumMatch, phi):
-    return Tree(phi);
-  elif re.match(binConnMatch, phi):
-    groups = re.match(binConnMatch, phi);
-    return Tree(groups.group(2), left = parseTree(groups.group(1)), right = parseTree(groups.group(3)));
-  elif re.match(unConnMatch, phi):
-    groups = re.match(unConnMatch, phi);
-    return Tree(groups.group(1), left = parseTree(groups.group(2)))
+binConnectives = u'↔|→|∨|∧';
+unConnective   = u'¬';
+atom            = u'p\d+';
+falsum          = u'⊥';
 
 def checkProp(phi):
+  #phi só tem símbolos do alfabeto de PROP
+  if re.search(u'[^↔→∨∧⊥¬p\d\(\)]', phi) != None:
+    return False;
+  #crear fila de espera
   queue = [];
-  queue.insert(0, phi)
+  queue.insert(0, phi);
+  #mientras a fila de espera tenha textos a ser evaluados fazer
   while(queue):
-    psi = queue.pop(0);
-    if re.match(atomicMatch, psi) or re.match(falsumMatch, psi):
+    #obter texto psi candidato a ser proposição
+    psi = queue.pop(0)
+    #test1 psi é atomico ou falsum 
+    if re.match(u'^(%s|%s)$'%(atom, falsum), psi):
       continue;
-    elif re.match(binConnMatch, psi):
-      groups = re.match(binConnMatch, psi);
-      queue.insert(0, groups.group(1));
-      queue.insert(0, groups.group(3));
-    elif re.match(unConnMatch, psi):
-      groups = re.match(unConnMatch, psi);
+     #test2 psi e uma negação
+    elif re.match(u'^\((%s)(.+)\)$'%(unConnective), psi):
+      groups = re.match(u'^\((%s)(.+)\)$'%(unConnective), psi);
       queue.insert(0, groups.group(2));
+    #test3 psi e uma proposição com conetiva central binaria
+    elif re.match(u'^\((.+)(%s)(.+)\)$'%(binConnectives), psi):
+      psi = psi[1:-1];
+      countLB = 0;
+      countRB = 0;
+      for i,s in enumerate(psi):
+        if re.match('%s'%(binConnectives), s):
+          if countLB == countRB:
+            queue.insert(0,psi[:i]);
+            queue.insert(0,psi[i+1:]);
+            break;
+        elif s == u'(':
+          countLB += 1;
+        elif s == u')':
+          countRB += 1 
+      if countLB != countRB:
+        return False;
     else:
       return False;
   return True;
 
 if __name__ == '__main__':
-  stringTree1 = '(p0↔(p1∧p0))'; 
-  stringTree2 = '(¬((p1∧p0)↔(p1∧p0)))';
-  stringTree3 = '((p1∧p0)↔((p1∧p0)→(¬p3)))';
-  stringTree4 = '(p0)';
-  stringTree5 = '((⊥';
-  stringTree6 = '(¬p0)';
+  d = {
+   'PROPS' : [
+      u'Hello world!',
+      u'p0 is an atom.',
+      u'p0p5p9p9p12',
+      u'((((p0↔(¬p13))))))',
+      u'(⊥)',
+      u'(p0)',
+      u'(p0↔⊥)',
+      u'(p0↔⊥)∨(p0↔⊥)',
+      u'((p0↔⊥)∨(p0↔⊥))',
+      u'p0',
+      u'(¬p0)',
+      u'(p0↔p40)',
+      u'(¬(p0↔p40))',
+      u'((¬(p0↔p40))∨(p1100∧⊥))',
+      u'⊥⊥',
+      u'((((p0→p1)→p2)→p0)→p15)',
+      u'(¬(¬(¬((((p0→p1)→p2)→p0)→p15))))',
+      u'((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))',
+      u'(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)',
+      u'((((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)→(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥))',
+      u'(((((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)→(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥))∨((((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)→(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)))',
+u'((((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)→(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥))∨((((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)→(((¬(¬(¬((((p0→p1)→p2)→p0)→p15))))∧(¬(¬(¬((((p0→p1)→p2)→p0)→p15)))))↔⊥)))',
+      u'(((p0→p1)∧(p2→p3))∨(¬p1∧¬p2))',
+      u'(((p0→p1)∧(p1→p2)∨(¬p0∧¬p1))',
+      u'((p0→p1)→(p1p1))p0))',
+      u'(((p0→p1)∧(p2→p3))∨((¬p1)∧(¬p2)))',
+    ],
+    'goldTag' : [
+      False,
+      False,
+      False,
+      False,
+      False,
+      False,
+      True,
+      False,
+      True,
+      True,
+      True,
+      True,
+      True,
+      True,
+      False,
+      True,
+      True,
+      True,
+      True,
+      True,
+      True,
+      False,
+      False,
+      False,
+      False,
+      True
+    ]
 
-  print('First tree');
-  if checkProp(stringTree1):
-    t = parseTree(stringTree1);
-    t.printTree();
+  }
 
-  print('Second tree');
-  if checkProp(stringTree2):
-    t = parseTree(stringTree2);
-    t.printTree();
+  df = pd.DataFrame(d);
+  df['isProp'] = df['PROPS'].apply(checkProp);
 
-  print('Third tree');
-  if checkProp(stringTree3):
-    t = parseTree(stringTree3);
-    t.printTree();
-
-  print('Fourth tree');
-  if checkProp(stringTree4):
-    t = parseTree(stringTree4);
-    t.printTree();
-
-  print('Fifth tree');
-  if checkProp(stringTree5):
-    t = parseTree(stringTree5);
-    t.printTree();
-
-  print('Sixth tree');
-  if checkProp(stringTree6):
-    t = parseTree(stringTree6);
-    t.printTree();
+  print(df);
+  print(f1_score(df['goldTag'], df['isProp']))
